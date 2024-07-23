5ea58b6fc2a9e9b744795b60b433eeb9d8b0835d
lukekeum <lukekeum11@gmail.com>
2024-07-23 18:52:33 +0900

    feat: add Header element

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import { Help, Letter, Logo } from '@/styles/Header';
+
+export default function Header() {
+  return (
+    <Wrapper>
+      <Div>
+        <Logo>Libro</Logo>
+        <Letter>대건고등학교 북카페 채팅 봇</Letter>
+        <Help />
+      </Div>
+    </Wrapper>
+  );
+}
+
+const Div = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 3rem 3.75rem;
+`;
+
+const Wrapper = styled.div`
+  width: 100vw;
+`;
---
+++ src/styles/Header.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Logo = styled.h1`
+  color: white;
+  font-weight: bold;
+  font-size: 2.25rem;
+  margin-left: 0.25rem;
+`;
+
+export const Letter = styled.p`
+  color: #c8c8c8;
+  font-size: 1.25rem;
+`;
+
+export const Help = styled.div`
+  width: 25px;
+  height: 25px;
+`;
