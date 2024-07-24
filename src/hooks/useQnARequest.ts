7b6d3c399cda5d5db89b9e1a297a3dc5b9856031
lukekeum <lukekeum11@gmail.com>
2024-07-24 17:18:01 +0900

    fix: change environment variable name

--- src/hooks/useQnARequest.ts
+++ src/hooks/useQnARequest.ts
@@ -22,7 +22,7 @@ export default function useQnARequest() {
       return [...prev, qna];
     });
 
-    const res = await axios.post(`${process.env.API_URL}`, {
+    const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}`, {
       text: qna.q,
     });
 
